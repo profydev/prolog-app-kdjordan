496557111f3a4d47fe75555becf2a4e904dba896
kdjordan <k.dean.jordan@gmail.com>
2024-02-29 18:41:44 -0800

    links in header laid out

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,43 @@
 import { Routes } from "@config/routes";
+// import classNames from "classnames";
+import Link from "next/link";
 import styles from "./index.module.scss";
 
 const IssuesPage = () => {
   return (
     <div>
       <header className={styles.header}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/icons/logo-large.svg" alt="Prolog logo" />
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link className={styles.anchor} href={Routes.home}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.anchor} href={Routes.products}>
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.anchor} href={Routes.documentation}>
+                Documentation
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.anchor} href={Routes.pricing}>
+                Pricing
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <a href={Routes.projects}>Dashboard</a>
+        </div>
       </header>
       <button
         className={styles.contactButton}
