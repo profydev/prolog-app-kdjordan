ac8a3bb835d8e608cbc3817e861fedfdc86458b5
Kevin Jordan <k.dean.jordan@gmail.com>
2024-03-03 19:31:32 -0800

    Add footer (#15)
    * footer placement not quite right
    
    * layout matching the design
    
    * added links to li
    
    * mobile footer done
    
    * refactor successfull
    
    * looking good - refactor done !
    
    * remove e3e footer test for now
    
    * added error boundary to project list test

---
+++ features/layout/page-footer/page-footer.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+// import { Routes } from "@config/routes";
+import styles from "./page-footer.module.scss";
+
+export default function PageFooter() {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const version = require("next/package.json").version;
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.content}>
+        <div className={styles.info}>Version: {version}</div>
+
+        <ul className={styles.links}>
+          <li>
+            <Link href="#">Docs</Link>
+          </li>
+          <li>
+            <Link href="#">API</Link>
+          </li>
+          <li>
+            <Link href="#">Help</Link>
+          </li>
+          <li>
+            <Link href="#">Community</Link>
+          </li>
+        </ul>
+
+        <div className={styles.logo}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={"/icons/logo-small.svg"} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
