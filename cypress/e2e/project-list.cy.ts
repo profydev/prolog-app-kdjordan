3170f0849a05257d6a5ec1ecec5d543da6040385
kdjordan <k.dean.jordan@gmail.com>
2024-03-04 20:21:23 -0800

    prettier error ?

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -28,7 +28,6 @@ describe("Project List", () => {
         .find("li")
         .each(($el, index) => {
           if (index < mockProjects.length) {
-
             // Add your assertions here using the mockProjects data
             cy.wrap($el).contains(mockProjects[index].name);
             cy.wrap($el).contains(languageNames[index]);
@@ -39,10 +38,8 @@ describe("Project List", () => {
               .find("a")
               .should("have.attr", "href", "/dashboard/issues");
           } else {
-
             console.log("each() function is not working as expected");
           }
-
         });
     });
   });
