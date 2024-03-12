3e2540c727ddcd1b3a9d69496dc882fb6ebe9744
kdjordan <k.dean.jordan@gmail.com>
2024-03-11 18:03:06 -0700

    sorybook working

--- features/ui/ui-button/ui-button.tsx
+++ features/ui/ui-button/ui-button.tsx
@@ -5,17 +5,12 @@ import classNames from "classnames";
 export function UIButton(props: ButtonProps) {
   const { title, size, color, disabled, icon, ...otherProps } = props;
 
-  {
-    /* eslint-disable-next-line @next/next/no-img-element */
-  }
-  console.log(color);
   // Define the class names based on the props
   const buttonClassNames = classNames(
     styles.button,
-    size && styles[size], // Assuming you have defined size classes in your SCSS
-    color && styles[color], // Assuming you have defined color classes in your SCSS
-    // state && styles[state], // Assuming you have defined state classes in your SCSS
-    icon && styles.icon, // Assuming you have defined icon class in your SCSS
+    size && styles[size],
+    color && styles[color],
+    icon && styles.icon,
   );
 
   const renderIconAndTitle = () => {
