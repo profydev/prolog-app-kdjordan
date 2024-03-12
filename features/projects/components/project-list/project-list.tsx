849c83c04b35ef6d3f7c21844ac0c363ae2da9cc
kdjordan <k.dean.jordan@gmail.com>
2024-03-12 06:29:26 -0700

    error alert working as expected

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -3,21 +3,18 @@ import { useGetProjects } from "../../api/use-get-projects";
 import styles from "./project-list.module.scss";
 
 export function ProjectList() {
-  const { data, isLoading, isError, error } = useGetProjects();
-  console.log(isError, error);
-  const test = 1;
+  const { data, isLoading, isError, error, refetch } = useGetProjects();
 
   function handleReload() {
-    console.log("do reload");
+    refetch();
   }
 
   if (isLoading) {
     return <div>Loading</div>;
   }
 
-  // if (isError) {
-  // {error.message}{
-  if (test == 1) {
+  if (isError) {
+    console.log(error.message);
     return (
       <div className={styles.error}>
         <div>
@@ -25,7 +22,7 @@ export function ProjectList() {
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src="/icons/exclaim.svg" alt="Alert !" />
           </div>
-          <div>There was a problem loading the project data</div>
+          <div>There was a problem while loading the project data</div>
         </div>
         <div onClick={handleReload}>
           <div>Try Again</div>
