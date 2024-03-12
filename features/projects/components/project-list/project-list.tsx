1d60a8480c8b10c560a2c41f12f821de3c6a8192
kdjordan <k.dean.jordan@gmail.com>
2024-03-11 20:07:12 -0700

    error div scaffolded out

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -4,14 +4,38 @@ import styles from "./project-list.module.scss";
 
 export function ProjectList() {
   const { data, isLoading, isError, error } = useGetProjects();
+  console.log(isError, error);
+  const test = 1;
+
+  function handleReload() {
+    console.log("do reload");
+  }
 
   if (isLoading) {
     return <div>Loading</div>;
   }
 
-  if (isError) {
-    console.error(error);
-    return <div>Error: {error.message}</div>;
+  // if (isError) {
+  // {error.message}{
+  if (test == 1) {
+    return (
+      <div className={styles.error}>
+        <div>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/icons/exclaim.svg" alt="Alert !" />
+          </div>
+          <div>There was a problem loading the project data</div>
+        </div>
+        <div onClick={handleReload}>
+          <div>Try Again</div>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/icons/arrow-right.svg" alt="Alert !" />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
